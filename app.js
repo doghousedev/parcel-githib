3d81b38a0a4bfe2891251751a3f8f07d19129b8f
Doghousedev <rmbaylin@gmail.com>
2022-02-21 15:17:18 -0500

    testing 1st time

---
+++ app.js
@@ -0,0 +1,10 @@
+import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+
+
+// index.js
+$(document).ready(()=>{
+  $('body').html('Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!');
+})
